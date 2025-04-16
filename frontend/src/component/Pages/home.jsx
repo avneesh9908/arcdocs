4b3dea6759583be03fe24843db51e60925b0a9d2
Avneesh kaushik <avikaushik000@gmail.com>
2025-04-16 09:22:32 +0530

    remove the repeated hero image

--- frontend/src/component/Pages/home.jsx
+++ frontend/src/component/Pages/home.jsx
@@ -139,19 +139,6 @@ const Home = () => {
                 </div>
               </div>
             </div>
-            {/* Single Slider */}
-            <div className="single-slider slider-height hero-overly d-flex align-items-center">
-              <div className="container">
-                <div className="row">
-                  <div className="col-xl-6 col-lg-6">
-                    <div className="hero__caption">
-                      <span data-animation="fadeInLeft" data-delay=".4s">Welcome to Intorior</span>
-                      <h1 data-animation="fadeInLeft" data-delay=".6s">Modern Interior & Design</h1>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
           </div>
           {/* Video icon */}
           <div className="video-icon">
